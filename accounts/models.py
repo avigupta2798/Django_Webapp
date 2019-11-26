0f544a801946a9459cd033467e5aab37887f084f
Avi Gupta <avibilasgupta@gmail.com>
2019-11-27 01:35:59 +0530

    name added

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User
 
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User,on_delete=models.PROTECT, null=True, blank=True)
-    portfolio_site = models.URLField(blank=True)
-    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=13)
     def __str__(self):
         return self.user.username
