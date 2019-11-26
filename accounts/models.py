753b4fb464cd81c4793ea69b42bd7a02c67e4804
Avi Gupta <avibilasgupta@gmail.com>
2019-11-27 00:06:04 +0530

    Webapp created Database created

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+# accounts/models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User,on_delete=models.PROTECT, null=True, blank=True)
+    portfolio_site = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+    def __str__(self):
+        return self.user.username
